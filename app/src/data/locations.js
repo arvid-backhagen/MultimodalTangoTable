90cd198e5e1952fc8ad8a2d36a917e05d547d99c
Ibz <ibrahim.asfadai@gmail.com>
2018-01-06 16:06:34 +0100

    Draft for desktop client

---
+++ app/src/data/locations.js
@@ -0,0 +1,38 @@
+export const TRACKS = [
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+	{
+		distance: "980 m",
+    name: 'Lustiga Huset',
+		provider: 'Gröna Lund',
+		countdown: new Date("2017-12-31"),
+	},
+]
