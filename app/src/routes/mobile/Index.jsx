e90cb39f7670e42d7c29cc6d4b87bf1e64e127f1
Ibz <ibrahim.asfadai@gmail.com>
2018-01-07 14:58:37 +0100

    initial mobile draft

--- app/src/routes/mobile/Index.jsx
+++ app/src/routes/mobile/Index.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+import daftPunkSmall from '../../img/small/daft-punk.jpg';
+
+import tangoTable from '../../img/tango-table.svg';
+import phone from '../../img/phone.svg';
+
 import '../../css/mobile.css';
 
 class Mobile extends Component {
@@ -52,38 +57,122 @@ class Mobile extends Component {
   }
 
   render() {
-    let { position, waveform } = this.state;
+    let specList = [];
 
-    let waveformList = null;
-    if (waveform) {
-      waveformList = [];
-
-      for (var i = 0; i <= Math.min(64, waveform.length); i++) {
-        let wave = waveform[i]
-        waveformList[i] = <div style={{ width: '10px', borderRadius: '5px 5px 0px 0px', height: wave, background:'#222' }}></div>
-
-      }
+    for (var i = 0; i < 12; i++) {
+      specList[i] = <div key={ i } className="spec-container">
+        <div className="high spec"></div>
+        <div className="middle spec"></div>
+        <div className="low spec"></div>
+      </div>
     }
 
     return (
       <div id="mobile">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <main>
+          <div id="welcome" className="view">
+            <figure className="cover-bg"></figure>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <figure className="circle left"></figure>
+            <figure className="circle right"></figure>
 
-        <div className="waveform" style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'space-between', height: '300px', paddingLeft: '30px', paddingRight: '30px' }}>
-          { waveformList }
-        </div>
-
-        <div style={{ padding: '30px' }}>
-          <div style={{ width: '100%', height: '10px', background: '#f0f1f5' }}>
-            <div style={{ width: `${position * 100}%`, height: '100%', background:'#222' }}></div>
+            <div className="content">
+              <h3>Welcome to TangoTable</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <a className="rounded-button">Let's start</a>
+            </div>
           </div>
-        </div>
+
+          <div id="library" className="view">
+            <header>
+              <h1>Library</h1>
+              <p>Select a track from the list below. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+            </header>
+
+            <div className="track-group">
+              <div className="track">
+                <figure className="cover">
+                  <img src={ daftPunkSmall } />
+                </figure>
+
+                <div className="info">
+                  <h5>All Night</h5>
+                  <p>Chance The Rapper</p>
+                </div>
+              </div>
+              
+              <div className="track">
+                <figure className="cover">
+                  <img src={ daftPunkSmall } />
+                </figure>
+
+                <div className="info">
+                  <h5>All Night</h5>
+                  <p>Chance The Rapper</p>
+                </div>
+              </div>
+
+              <div className="track">
+                <figure className="cover">
+                  <img src={ daftPunkSmall } />
+                </figure>
+
+                <div className="info">
+                  <h5>All Night</h5>
+                  <p>Chance The Rapper</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="waiting" className="view">
+            <figure className="cover">
+            </figure>
+
+            <figure className="circle left"></figure>
+            <figure className="circle right"></figure>
+
+            <div className="content">
+              <div className="info">
+                <h3>Start playing music</h3>
+                <p>Place your phone on the table to start playing music, move your phone vertically to adjust the volume and lift your phone to pause the music.</p>
+              </div>
+            </div>
+
+            <figure className="illustration">
+              <img className="phone" src={ phone } />
+              <img src={ tangoTable } />
+            </figure>
+          </div>
+
+          <div id="music-player" className="view">
+            <figure className="cover">
+              <div ref={ (waveform) => { this.waveform = waveform; } } id="waveform">{ specList }</div>
+            </figure>
+
+            <div className="content">
+              <div className="info">
+                <h1>Around the World</h1>
+                <h4>Daft punk</h4>
+
+                <a class="rounded-button">Change SONG</a>
+              </div>
+
+              <h5>Lift your phone to pause the music</h5>
+
+              <h5 ref={ (timestamp) => { this.timestampCurrent = timestamp; } } className="time-stamp current">02:50</h5>
+              <h5 ref={ (timestamp) => { this.timestampEnd = timestamp; } } className="time-stamp end">02:50</h5>
+
+              <div className="progress-bar">
+                <div ref={ (bar) => { this.progressBar = bar; } } className="progress-fill"></div>
+              </div>
+            </div>
+
+            <figure className="illustration">
+              <img src={ daftPunkSmall } />
+            </figure>
+          </div>
+        </main>
       </div>
     );
   }
